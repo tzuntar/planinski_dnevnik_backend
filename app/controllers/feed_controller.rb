4ea91be8bb63fca346d8c40dcc71f3e64c240f89
Tobija Žuntar <tobija.zuntar@gmail.com>
2025-12-14 15:43:26 +0100

    Fix feed fetching

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,15 +2,11 @@ class FeedController < ApplicationController
   before_action :authorized
 
   def get
-    JournalEntry.where("is_public = 1 AND user_id != ?", current_user.id)
-                .includes(:users, :peaks)
-                .offset((params[:page] || 0) * (params[:limit] || 10))
-                .limit(params[:limit] || 10)
-                .map do |entry|
-      {
-        user_name: entry.user.name,
-        **rest
-      }
-    end
+    entries = JournalEntry.where("is_public = 1 AND user_id != ?", current_user.id)
+      .includes(:user, :peak)
+      .offset((params[:page] || 0) * (params[:limit] || 10))
+      .limit(params[:limit] || 10)
+    # Rails.logger.info entries.inspect
+    render json: entries
   end
 end
