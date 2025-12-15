47ce91edec071ec9d9c0a2a36cf57824db03b992
Tobija Žuntar <tobija.zuntar@gmail.com>
2025-12-15 21:41:51 +0100

    Add weather string to journal entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_14_135233) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_15_204004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_14_135233) do
     t.string "photo_path"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.text "weather"
     t.index ["peak_id"], name: "index_journal_entries_on_peak_id"
     t.index ["user_id"], name: "index_journal_entries_on_user_id"
   end
