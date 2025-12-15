037eb2fcf26260d45e80fd1d45d761c972dd9b39
dmarkslo <mark.david.horvat@gmail.com>
2025-12-15 21:42:36 +0100

    Added update bio function in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,16 @@ class UsersController < ApplicationController
     render json: { user: current_user }
   end
 
+  def update_bio
+    user = { user: current_user }
+    
+    if user.update(bio: params[:bio])
+      render json: { message: "success", bio: user.bio }, status: :ok
+    else
+      render json: { error: "Napaka pri shranjevanju bio-ta" }, status: :unprocessable_entity
+    end
+  end
+
   # GET /users/:id
   def show
     user = User.includes(:journal_entries).find(params[:id])
