68734b9037d34af413338b4d05d1602125397bad
Tobija Žuntar <tobija.zuntar@gmail.com>
2025-12-20 21:20:34 +0100

    Include user and peak data in feed post responses

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -7,11 +7,14 @@ class FeedController < ApplicationController
       .where("is_public = 1 AND user_id != ?", current_user.id)
       .offset((params[:page] || 0) * (params[:limit] || 10))
       .limit(params[:limit] || 10)
-    # Rails.logger.info @entries.inspect
     render json: entries.as_json(
-      includes: {
+      include: {
+        peak: {
+          include: {
+            country: { only: [:id, :name] }
+          }
+        },
         user: { only: [ :id, :name, :photo_path ] },
-        peak: { only: [ :id, :name ] }
       }
     )
   end
