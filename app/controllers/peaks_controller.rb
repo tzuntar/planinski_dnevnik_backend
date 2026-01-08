a3623d6930cee6045413832fddbf3ac6537ed550
Tobija Žuntar <tobija.zuntar@gmail.com>
2026-01-08 20:36:47 +0100

    Peak profile pages

--- app/controllers/peaks_controller.rb
+++ app/controllers/peaks_controller.rb
@@ -4,4 +4,16 @@ class PeaksController < ApplicationController
   def index
     render json: Peak.all
   end
+
+  def show
+    @entries = JournalEntry.where(peak_id: params[:id])
+                           .includes(:user)
+                           .offset((params[:page] || 0) * (params[:limit] || 10))
+                           .limit(params[:limit] || 10)
+    render json: @entries.as_json(
+      include: {
+        user: {}
+      }
+    )
+  end
 end
